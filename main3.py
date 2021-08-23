c94f460aa17ba1d3f4a20ccb8bce09de8cb262dd
John Robles <john.robles1@upr.edu>
2021-08-23 14:32:50 -0400

    cleaning code

--- main3.py
+++ main3.py
@@ -1,9 +1,6 @@
 import qrcode
 
+# making the QR Code
 img = qrcode.make("https://docs.google.com/forms/d/e/1FAIpQLSdfj6yuP1FFBPjPSocwBfeAW51G-0K-qdXBDLqTr5jjXSJZ_w/viewform?usp=sf_link")
-# img.save("/Users/jrobles/Documents/QR Code/code.jpg")
-print("the type of image is: ",type(img))
+# saving the QR Code in an image
 img.save("Tutorias-Diana.png")
-# img.save("some_file.svg")
-# with open('myfile.png', 'wb') as f:
-#     img.save(f)
